9284e8484ee5627dea6b768cb81a4c2df4607be8
unknown <jeffersonsantos.a@gmail.com>
2020-09-25 09:35:52 -0300

    pequens alterções na estrutura do app

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
 	<v-app id="inspire">
 		<v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" app>
+			<v-list-item two-line :class="miniVariant && 'px-0'">
+				<v-list-item-avatar>
+					<img src="https://randomuser.me/api/portraits/men/81.jpg" />
+				</v-list-item-avatar>
+
+				<v-list-item-content>
+					<v-list-item-title>Application</v-list-item-title>
+					<v-list-item-subtitle>Subtext</v-list-item-subtitle>
+				</v-list-item-content>
+			</v-list-item>
+
+			<v-divider></v-divider>
 			<v-list dense v-for="(link, index) in navLinks" :key="index">
 				<v-list-item :to="link.path">
 					<v-list-item-action>
-						<v-icon class="menu-icon">{{link.icon}}</v-icon>
+						<v-icon class="menu-icon">{{ link.icon }}</v-icon>
 					</v-list-item-action>
 					<v-list-item-content>
 						<v-list-item-title>{{ link.text }}</v-list-item-title>
@@ -13,18 +25,27 @@
 			</v-list>
 		</v-navigation-drawer>
 
-		<v-app-bar app color="indigo" dark>
-			<v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-			<v-icon large class="account-icon" v-if="$route.name === 'Paciente'">mdi-account</v-icon>
-			<v-toolbar-title v-if="$route.name === 'Paciente'" v-text="this.$store.state.patient.name"></v-toolbar-title>
+		<v-app-bar app color="primary" dark>
+			<v-app-bar-nav-icon
+				@click.stop="drawer = !drawer"
+			></v-app-bar-nav-icon>
+			<v-icon large class="account-icon" v-if="$route.name === 'Paciente'"
+				>mdi-account</v-icon
+			>
+			<v-toolbar-title
+				v-if="$route.name === 'Paciente'"
+				v-text="this.$store.state.patient.name"
+			></v-toolbar-title>
 			<v-toolbar-title v-else v-text="$route.name"></v-toolbar-title>
 		</v-app-bar>
 
 		<v-main class="content">
 			<Content data-app />
 		</v-main>
-		<v-footer color="indigo" app :inset="true">
-			<span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
+		<v-footer color="primary" app :inset="true">
+			<span class="white--text"
+				>&copy; {{ new Date().getFullYear() }}</span
+			>
 		</v-footer>
 	</v-app>
 </template>
@@ -36,8 +57,8 @@ export default {
 	name: "App",
 	components: { Content },
 	data: () => ({
-        drawer: null,
-        miniVariant: false,
+		drawer: null,
+		miniVariant: false,
 		navLinks: [
 			{
 				text: "Home",
@@ -73,11 +94,10 @@ body {
 	margin: 0;
 }
 .content {
-        background-color: #F1F6FC;
-        padding: 20px;
-    }
-.account-icon{
+	background-color: #f1f6fc;
+	padding: 20px;
+}
+.account-icon {
 	margin-right: 10px;
 }
-
 </style>
