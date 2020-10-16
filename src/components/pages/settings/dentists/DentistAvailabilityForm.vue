d1df61a0a9e68b6b8188d25121a60fad3a238e6d
jeffersondossantosaguiar <jeffersonsantos.a@gmail.com>
2020-10-16 08:12:34 -0300

    Pagina de disponibilidade

--- src/components/pages/settings/dentists/DentistAvailabilityForm.vue
+++ src/components/pages/settings/dentists/DentistAvailabilityForm.vue
@@ -1,36 +1,86 @@
 <template>
-	<div class="patient-form">
-		<v-dialog :value="value" :editedDentist="editedDentist" persistent width="900px">
-            <v-card>
-                <v-card-title>
-                    Disponibilidade
-                    <v-spacer></v-spacer>
-					<v-btn icon @click="closeDialog">
-						<v-icon>mdi-close</v-icon>
-					</v-btn>
-                </v-card-title>
-                <v-card-text>ABC</v-card-text>
-            </v-card>
-		</v-dialog>
-	</div>
+  <div class="patient-form">
+    <v-dialog
+      :value="value"
+      :editedDentist="editedDentist"
+      persistent
+      width="900px"
+    >
+      <v-card>
+        <v-card-title>
+          Disponibilidade
+          <v-spacer></v-spacer>
+          <v-btn icon @click="closeDialog">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-card-title>
+        <v-card-text>
+          <v-row class="availability-days">
+            <v-col class="availability-cols">
+              <v-btn color="red" fab small dark>
+                D
+              </v-btn>
+            </v-col>
+            <!-- <v-select
+              :items="items"
+              append-icon="mdi-clock"
+              label="Entrada"
+              dense
+              outlined
+            ></v-select> -->
+          </v-row>
+          <v-row class="availability-days">
+            <v-btn color="primary" fab small dark>
+              S
+            </v-btn>
+          </v-row>
+          <v-row class="availability-days">
+            <v-btn color="primary" fab small dark>
+              T
+            </v-btn>
+          </v-row>
+          <v-row class="availability-days">
+            <v-btn color="primary" fab small dark>
+              Q
+            </v-btn>
+          </v-row>
+          <v-row class="availability-days">
+            <v-btn color="primary" fab small dark>
+              Q
+            </v-btn>
+          </v-row>
+          <v-row class="availability-days">
+            <v-btn color="primary" fab small dark>
+              S
+            </v-btn>
+          </v-row>
+          <v-row class="availability-days">
+            <v-btn color="primary" fab small dark>
+              S
+            </v-btn>
+          </v-row>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
-
 export default {
-	name: "patient-form",
-	props: ["value", "editedDentist"],
-	data() {
-		return {		
-			valid: true,
-		};
-	},
-	methods: {	
-        closeDialog() {
-			this.$emit("input");
-		},
-	},
-/* 	watch: {
+  name: "patient-form",
+  props: ["value", "editedDentist"],
+  data() {
+    return {
+      valid: true,
+      items: ["", "Foo", "Bar", "Fizz", "Buzz"],
+    };
+  },
+  methods: {
+    closeDialog() {
+      this.$emit("input");
+    },
+  },
+  /* 	watch: {
 		editedDentist: function (editedDentist) {
 			this.paciente = editedDentist;
 		},
@@ -38,4 +88,11 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.availability-days {
+  margin: 10px;
+}
+.availability-cols{
+	padding: 0;
+}
+</style>
