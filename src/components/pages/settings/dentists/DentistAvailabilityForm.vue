77f7b24e2c80284b8f20d75474cc73345e078bc5
jeffersondossantosaguiar <jeffersonsantos.a@gmail.com>
2020-10-16 16:28:08 -0300

    Form de disponibilidade

--- src/components/pages/settings/dentists/DentistAvailabilityForm.vue
+++ src/components/pages/settings/dentists/DentistAvailabilityForm.vue
@@ -4,7 +4,7 @@
       :value="value"
       :editedDentist="editedDentist"
       persistent
-      width="900px"
+      max-width="900px"
     >
       <v-card>
         <v-card-title>
@@ -15,69 +15,131 @@
           </v-btn>
         </v-card-title>
         <v-card-text>
-          <v-row class="availability-days">
-            <v-col class="availability-cols">
-              <v-btn color="red" fab small dark>
-                D
-              </v-btn>
-            </v-col>
-            <!-- <v-select
-              :items="items"
-              append-icon="mdi-clock"
-              label="Entrada"
-              dense
-              outlined
-            ></v-select> -->
-          </v-row>
-          <v-row class="availability-days">
-            <v-btn color="primary" fab small dark>
-              S
+          <v-row v-for="(day, index) in availabilityDaysArray" :key="index" class="availabilityDays">
+            <v-btn :color="day.disabled ? `red` : `primary`" fab small dark>
+              {{ day.day }}
             </v-btn>
-          </v-row>
-          <v-row class="availability-days">
-            <v-btn color="primary" fab small dark>
-              T
-            </v-btn>
-          </v-row>
-          <v-row class="availability-days">
-            <v-btn color="primary" fab small dark>
-              Q
-            </v-btn>
-          </v-row>
-          <v-row class="availability-days">
-            <v-btn color="primary" fab small dark>
-              Q
-            </v-btn>
-          </v-row>
-          <v-row class="availability-days">
-            <v-btn color="primary" fab small dark>
-              S
-            </v-btn>
-          </v-row>
-          <v-row class="availability-days">
-            <v-btn color="primary" fab small dark>
-              S
-            </v-btn>
+
+            <vue-timepicker
+              v-model="day.start_time"
+              placeholder="Início Jornada"
+              :disabled="day.disabled"
+              manual-input
+              hide-dropdown
+            ></vue-timepicker
+            >:
+
+            <vue-timepicker
+              v-model="day.start_break_hour"
+              placeholder="Início Descanso"
+              :disabled="day.disabled"
+              manual-input
+              hide-dropdown
+            ></vue-timepicker
+            >:
+
+            <vue-timepicker
+              v-model="day.end_break_hour"
+              placeholder="Final Descanso"
+              :disabled="day.disabled"
+              manual-input
+              hide-dropdown
+            ></vue-timepicker
+            >:
+
+            <vue-timepicker
+              v-model="day.end_time"
+              placeholder="Final Jornada"
+              :disabled="day.disabled"
+              manual-input
+              hide-dropdown
+            ></vue-timepicker
+            >
           </v-row>
         </v-card-text>
+        <v-card-actions>
+					<v-spacer></v-spacer>
+					<v-btn :disabled="!valid" color="blue darken-1" text type="submit">Salvar</v-btn>
+				</v-card-actions>
       </v-card>
     </v-dialog>
   </div>
 </template>
 
 <script>
+import VueTimepicker from "vue2-timepicker"
+import "vue2-timepicker/dist/VueTimepicker.css"
+
 export default {
   name: "patient-form",
   props: ["value", "editedDentist"],
+  components: { VueTimepicker },
   data() {
     return {
       valid: true,
-      items: ["", "Foo", "Bar", "Fizz", "Buzz"],
-    };
+      availabilityDaysArray: [
+        {
+          day: "D",
+          disabled: true,
+          start_time: { HH: "", mm: "" },
+          start_break_hour: { HH: "", mm: "" },
+          end_break_hour: { HH: "", mm: "" },
+          end_time: { HH: "", mm: "" },
+        },
+        {
+          day: "S",
+          disabled: false,
+          start_time: { HH: "", mm: "" },
+          start_break_hour: { HH: "", mm: "" },
+          end_break_hour: { HH: "", mm: "" },
+          end_time: { HH: "", mm: "" },
+        },
+        {
+          day: "T",
+          disabled: false,
+          start_time: { HH: "", mm: "" },
+          start_break_hour: { HH: "", mm: "" },
+          end_break_hour: { HH: "", mm: "" },
+          end_time: { HH: "", mm: "" },
+        },
+        {
+          day: "Q",
+          disabled: false,
+          start_time: { HH: "", mm: "" },
+          start_break_hour: { HH: "", mm: "" },
+          end_break_hour: { HH: "", mm: "" },
+          end_time: { HH: "", mm: "" },
+        },
+        {
+          day: "Q",
+          disabled: false,
+          start_time: { HH: "", mm: "" },
+          start_break_hour: { HH: "", mm: "" },
+          end_break_hour: { HH: "", mm: "" },
+          end_time: { HH: "", mm: "" },
+        },
+        {
+          day: "S",
+          disabled: false,
+          start_time: { HH: "", mm: "" },
+          start_break_hour: { HH: "", mm: "" },
+          end_break_hour: { HH: "", mm: "" },
+          end_time: { HH: "", mm: "" },
+        },
+        {
+          day: "S",
+          disabled: false,
+          start_time: { HH: "", mm: "" },
+          start_break_hour: { HH: "", mm: "" },
+          end_break_hour: { HH: "", mm: "" },
+          end_time: { HH: "", mm: "" },
+        },
+      ],
+    }
   },
   methods: {
     closeDialog() {
-      this.$emit("input");
+      this.$emit("input")
     },
   },
   /* 	watch: {
@@ -85,14 +147,26 @@ export default {
 			this.paciente = editedDentist;
 		},
 	}, */
-};
+}
 </script>
 
 <style>
-.availability-days {
-  margin: 10px;
+.row {
+  padding: 5px;
 }
-.availability-cols{
-	padding: 0;
+.availabilityDays{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 10px;
+}
+.v-btn {
+  margin-right: 20px;
+}
+.disabled {
+  background: #d0cece;
+}
+.display-time {
+  border-radius: 4px;
 }
 </style>
